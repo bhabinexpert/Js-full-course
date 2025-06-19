33d4e217458550b24e292016ed3e482d9d55791d
Bhabin Dulal <bhabindulal35@gmail.com>
2025-06-19 14:02:46 +0545

    initial commit

---
+++ nestedLoop.js
@@ -0,0 +1,12 @@
+// Started the tutorial //
+
+console.log("hello World!!")
+
+// nested for loop:
+
+for (let i = 1; i <=10; i++){
+    for (let j = 1; j <=10; j++){
+        console.log(`${i} * ${j} = ${i * j}`)
+    }
+
+}
