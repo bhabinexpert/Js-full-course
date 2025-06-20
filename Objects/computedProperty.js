8f38e74c3953d125abfd4da711fd32264dda0967
Bhabin Dulal <bhabindulal35@gmail.com>
2025-06-20 21:36:39 +0545

    obj

---
+++ Objects/computedProperty.js
@@ -0,0 +1,18 @@
+// * Adding property to object
+
+// const readLineSync = require("readline-sync")
+// const key = readLineSync.question("what do you wan to know? name/ age/ city")
+
+
+
+
+const obj = {
+    name: "vabin",
+    age: 19,
+
+}
+obj.city ="jhapa"
+obj.country = "Nepal"
+console.log(obj)
+
+// console.log(obj[key])
