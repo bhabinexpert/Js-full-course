701e7138d94ca8866a269898acceca855cdaa4c1
Bhabin Dulal <bhabindulal35@gmail.com>
2025-06-25 21:53:41 +0545

    coisting

---
+++ hoisting/hoisting-let,const.js
@@ -0,0 +1,7 @@
+
+
+console.log(x) // can't acess before execution
+let x = 9;
+console.log(y) // undefined
+var y = 10;
+console.log(y) //10
---
+++ hoisting/hoisting-var.js
@@ -0,0 +1,11 @@
+// hoisting refers to the acess of variables or function before it's initialization
+
+var age = 25;
+console.log(age)
+
+showName()
+console.log(showName())
+
+function showName(){
+    console.log("Bhabin")
+}
